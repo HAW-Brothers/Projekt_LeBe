eb0b5d235bec4dc36b41c32472ec7c6e16af4199
Abraxas213 <Florian.Pietrzak@haw-hamburg.de>
2016-12-21 17:18:51 +0100

    Fixing nach letzten Merge im CVAMarket

--- app/src/main/java/lebe/lebeprototyp02/market/CVAMarket.java
+++ app/src/main/java/lebe/lebeprototyp02/market/CVAMarket.java
@@ -1,14 +1,18 @@
 package lebe.lebeprototyp02.market;
 
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +37,11 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import lebe.lebeprototyp02.ApplicationDetail;
+import lebe.lebeprototyp02.MainActivity;
 import lebe.lebeprototyp02.Internetz.DownloadCounter;
 import lebe.lebeprototyp02.R;
 
@@ -46,13 +54,13 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
     private Context mContext;
     private ArrayList<MarketItem> itemListe;
 
-
+    private List<ApplicationDetail> applications;
 
     public CVAMarket(Context context, ArrayList<MarketItem> liste) {
         super(context, R.layout.market_single_item, liste);
         mContext = context;
-        itemListe= liste;
-
+        itemListe = liste;
+        this.applications = loadApplication();
 
 
     }
@@ -65,7 +73,7 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
      * @param parent
      * @return
      */
-    public View getView(int position, View convertview, ViewGroup parent){
+    public View getView(int position, View convertview, ViewGroup parent) {
 
 
         final int poosition=position;
@@ -78,12 +86,24 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
         ImageView logo = (ImageView) rowViev.findViewById(R.id.market_logo);
         TextView name = (TextView) rowViev.findViewById(R.id.market_Name);
         TextView beschreibung = (TextView) rowViev.findViewById(R.id.market_beschreibung);
-        Button download = (Button) rowViev.findViewById(R.id.market_download);
-
+        final Button download = (Button) rowViev.findViewById(R.id.market_download);
 
 
         //MarketItem Objekt erzeugen
         MarketItem item = itemListe.get(position);
+
+                /*
+         Identifiziert, ob das Plug-In bereits installiert ist. Wenn ja, setze den Flag
+         */
+        boolean flag = false;
+        for(int i = 0 ; i< this.applications.size() ; i ++){
+            if(applications.get(i).getLabel().toString().equals(item.getName())){
+                flag = true;
+                break;
+            }
+        }
+
+
 
         final String tempDateiname = item.getName();
         final String tempdownload = item.getDdlpath();
@@ -91,7 +111,28 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i;
+
+                                /*
+                Prüft ob das Plug-In bereits installiert isz.
+                 */
+                List<ApplicationDetail> plugIns = loadApplication();
+
+                boolean flag = false;
+                for(int index = 0 ; index< plugIns.size() ; index++){
+                    if(plugIns.get(index).getLabel().toString().equals(tempDateiname)){
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if(flag){
+                    download.setText("Installiert");
+                    download.setEnabled(false);
+                } else {
+
+
+
+                    Intent i;
                 PackageManager manager = getContext().getPackageManager();
                 try {
                     i = manager.getLaunchIntentForPackage("lebe.lebeprototyp02");
@@ -118,8 +159,8 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
 //                    tempdownload.replaceAll("\\","");
 //                    System.out.println(tempdownload);
                     count(tempDateiname);
-                    downloadAndInstall.execute(tempdownload, tempDateiname+".apk");
-
+                    downloadAndInstall.execute(tempdownload, tempDateiname + ".apk");
+                }
 
 
 
@@ -129,7 +170,13 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
 
         });
 
-
+                /*
+        Fals dieses Plug-In bereits installiert ist, ist es nicht mehr downloadbar;
+         */
+        if(flag){
+            download.setText("Installiert");
+            download.setEnabled(false);
+        }
 
         name.setText(item.getName());
 
@@ -160,5 +207,31 @@ public class CVAMarket extends ArrayAdapter<MarketItem> {
     }
 
 
+    /**
+     * Läd die vorhandenen Plug-Ins, um abzugleichen ob diese bereits installiert sind.
+     * @return
+     */
+    private List loadApplication() {
+        // package manager is used to retrieve the system's packages
+        PackageManager packageManager = ((MainActivity)getContext()).getPackageManager();
+        List applications = new ArrayList<>();
+        // we need an intent that will be used to load the packages
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        // in this case we want to load all packages wtih PLUGIN category
+        intent.addCategory("lebe.PLUGIN");
+        List<ResolveInfo> availableActivities = packageManager.queryIntentActivities(intent, 0);
+        // for each one we create a custom list view item
+        for (ResolveInfo resolveInfo : availableActivities) {
+            ApplicationDetail applicationDetail = new ApplicationDetail(
+                    resolveInfo.loadLabel(packageManager),
+                    resolveInfo.activityInfo.packageName,
+                    resolveInfo.activityInfo.loadIcon(packageManager));
+            applications.add(applicationDetail);
+        }
+
+        return applications;
+
+
+    }
 
 }
